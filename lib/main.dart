a7b0cf3b6fb78aa2d1f77fd9d06f6934455c7eb0
Ionut-Valentin Mitrache <ionutvalentin66@gmail.com>
2022-01-20 14:23:41 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web Views',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          fontFamily: "Arial",
+          textTheme: TextTheme(
+              button: TextStyle(color: Colors.white, fontSize: 18.0))),
+      home: Home(),
+    );
+  }
+}
